eb5a3afc9af3e9069ae558388b52c5ffef1b469c
Xenofan-git <romansysoev.57@gmail.com>
2026-09-19 19:03:27 +0300

    Test Keenetic CSQTT defaults against upstream policy

--- main_test.go
+++ main_test.go
@@ -10,3 +10,13 @@ func TestAutoCallCountMatchesAndroidPolicy(t *testing.T) {
 		}
 	}
 }
+
+func TestDefaultsMatchOriginalCSQTT(t *testing.T) {
+	var c Config
+	defaults(&c)
+	if c.VKHashMode != "auto_api" { t.Fatalf("VK hash mode=%q, want auto_api", c.VKHashMode) }
+	if c.Obfs != "video" { t.Fatalf("obfs=%q, want video", c.Obfs) }
+	if c.Fingerprint != "firefox" { t.Fatalf("fingerprint=%q, want firefox", c.Fingerprint) }
+	if c.ClientIDs != "8202606,6287487" { t.Fatalf("client_ids=%q, want original pair", c.ClientIDs) }
+	if c.VKAuthMode != "vkcalls" { t.Fatalf("VK auth mode=%q, want vkcalls", c.VKAuthMode) }
+}
